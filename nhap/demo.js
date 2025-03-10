d6256aa784ce73ea94e27556584fca01e53c32bf
TAi <daudangvua@gmail.com>
2025-03-10 22:21:01 +0700

    add bt5/

---
+++ nhap/demo.js
@@ -0,0 +1,10 @@
+// console.log("Hello anh em!");
+// alert("Hello anh em!");
+// document.write('Xin chào!');
+// document.getElementById("demo").innerText = "<h3>Hello anh em anh em</h3>";
+// document.getElementById("demo2").innerHTML = "<h3>Hello anh em anh em</h3>";
+
+// name = prompt("nhap")
+// document.getElementById("name").innerText = name;
+// lấy từ ô input
+function getName()
