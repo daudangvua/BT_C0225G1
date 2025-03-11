1d142d03830cda5bcd73da03c63493cad08282b4
TAi <daudangvua@gmail.com>
2025-03-11 18:20:42 +0700

    add bai7/

--- bai7/bt1_1.js
+++ bai7/bt1_1.js
@@ -2,10 +2,10 @@ function convert(){
     var vatly = parseFloat( document.getElementById("vatly").value);
     var hoahoc = parseFloat( document.getElementById("hoahoc").value);
     var sinhhoc =parseFloat( document.getElementById("sinhhoc").value);
-
     tong = vatly+hoahoc+sinhhoc
     trungbinh = tong/3
     document.getElementById("tong").innerHTML = "tong: " + tong;
     document.getElementById("trungbinh").innerHTML = trungbinh;
 }
 
+7
