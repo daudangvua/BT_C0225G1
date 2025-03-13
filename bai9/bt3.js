9eb107fb9656f1613c938d49fe48ee4f7c6df05a
TAi <daudangvua@gmail.com>
2025-03-13 10:24:27 +0700

    add bai9/

--- bai9/bt3.js
+++ bai9/bt3.js
@@ -1,4 +1,10 @@
-function inputvalue(inq) {
-    let nhap = document.getElementById('nhap');
-    nhap.value += inq;
+function nhaps(num) {
+    document.getElementById("nhap").value += num;
+}
+function xoa() {
+    document.getElementById('nhap').value= "";
+}
+function bang() {
+   let tinh = document.getElementById('nhap').value;
+   document.getElementById('nhap').value= eval(tinh);
 }
