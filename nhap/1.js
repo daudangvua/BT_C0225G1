6f3358bb0d5887f401152c61f3b242536c522e0b
TAi <daudangvua@gmail.com>
2025-03-17 20:13:55 +0700

    add all

--- nhap/1.js
+++ nhap/1.js
@@ -1,24 +1,38 @@
 
-let ten = prompt("nhap ten cua ban");
-if (ten !="") {
-    alert("xin chao: " +ten);
+// let ten = prompt("nhap ten cua ban");
+// if (ten !="") {
+//     alert("xin chao: " +ten);
+// }
+//
+// let chan = prompt("nhap so");
+// if (chan %2==0) {
+//     alert("so chan");
+// }
+// else {
+//     alert("so le");
+// }
+//
+// let ss = prompt("nhap so");
+// if (ss >= 5) {
+//     alert("lon hon 5");
+// }
+// else if (ss = 5) {
+//     alert("la so 5");
+// }
+// else {
+//     alert("nho hon 5");
+// }
+let ketqua =""
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        ketqua += "FizzBuzz";
+    } else if (i % 3 === 0) {
+        ketqua += "Fizz";
+    } else if (i % 5 === 0) {
+        ketqua += "Buzz";
+    } else {
+        ketqua += i;
+    }
+    ketqua += "<br/>"
 }
-
-let chan = prompt("nhap so");
-if (chan %2==0) {
-    alert("so chan");
-}
-else {
-    alert("so le");
-}
-
-let ss = prompt("nhap so");
-if (ss >= 5) {
-    alert("lon hon 5");
-}
-else if (ss = 5) {
-    alert("la so 5");
-}
-else {
-    alert("nho hon 5");
-}
+document.getElementById("ketqua").innerHTML = ketqua
