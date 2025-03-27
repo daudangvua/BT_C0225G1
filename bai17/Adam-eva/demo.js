9c284f6fd63f77aa828f145b5335c04246558679
TAi <daudangvua@gmail.com>
2025-03-27 15:31:18 +0700

    add bai17/

--- bai17/Adam-eva/demo.js
+++ bai17/Adam-eva/demo.js
@@ -1,35 +1,47 @@
 class Apple {
     weight;
-    constructor(weight=10) {
+    constructor(weight) {
         this.weight=weight;
     }
-
+    isEmpty() {
+        return this.weight <= 0;
+    }
 
 }
 class Human {
     name;
     gender;
     weight;
-    speak;
     constructor(name, gender, weight, speak) {
         this.name = name;
         this.gender = gender;
         this.weight = weight;
-        this.speak = "";
     }
-    get name(){
-        return this.name;
+    call(mess){
+        console.log(`${this.name}: ${mess}`);
     }
-    set name(newName){
-        this.name = newName;
-    }
-    get gender(){
-        return this.gender;
-    }
-    set gender(newGender){
-        this.gender = newGender;
-    }
-    get weight(){
-        return this.weight;
+    eat(apple){
+        if (!apple.isEmpty()) {
+            apple.weight -= 1;
+            this.weight += 1;
+            console.log(`🍏 ${this.name} ăn táo!`);
+            console.log(`👉 Cân nặng ${this.name}: ${this.weight}kg | 🍏 Táo còn lại: ${apple.weight}`);
+        } else console.log(`hết táo rồi`);
     }
 }
+let apple = new Apple(10);
+let Adam = new Human('Adam', 'male', 50 );
+let Eva = new Human('Eva', 'female', 50 );
+console.log(`🍏 Trọng lượng táo ban đầu: ${apple.weight}`);
+console.log(`🏋️‍♂️ Cân nặng Adam: ${Adam.weight}kg | 🏋️‍♀️ Cân nặng Eva: ${Eva.weight}kg`);
+console.log("===== BẮT ĐẦU ĂN TÁO =====");
+while (!apple.isEmpty()) {
+    Adam.eat(apple);
+    if (!apple.isEmpty()) {
+        Eva.eat(apple);
+    }
+}
+
+console.log("===== HẾT TÁO =====");
+console.log(`🏋️‍♂️ Cân nặng cuối cùng của Adam: ${Adam.weight}kg`);
+console.log(`🏋️‍♀️ Cân nặng cuối cùng của Eva: ${Eva.weight}kg`);
---
+++ bai17/bt-Cat-Rat/index.js
@@ -0,0 +1,6 @@
+let m1 = new Cat("Tam", 82, 80);
+let m2 = new Cat("KA", 44, 30);
+let c1 = new Rat("Khanh", 61, 50, true);
+let c2 = new Rat("Khanh Pham", 60, 40, true);
+m1.catch(c1);
+m2.catch(c2);
